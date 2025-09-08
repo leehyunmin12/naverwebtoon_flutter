61065ae3d5c13afb514b9cdc90d0f1bc9ea3f732
leehyunmin <leehm2292@gmail.com>
2025-09-08 09:15:10 +0900

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
+        //상위 바
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(50),
 
@@ -22,22 +23,101 @@ class MyApp extends StatelessWidget {
               bottom: BorderSide(color: Colors.grey.shade300, width: 1),
             ),
             title: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("웹툰", style: TextStyle(fontSize: 20)),
-                SizedBox(width: 8),
-                Text(
-                  "알아서 딱!",
-                  style: TextStyle(fontSize: 13, color: Colors.grey.shade500),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("웹툰", style: TextStyle(fontSize: 20)),
+                    SizedBox(width: 8),
+                    Text(
+                      "알아서 딱!",
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_downward,
+                      size: 14,
+                      color: Colors.grey.shade500,
+                    ),
+                  ],
                 ),
-                Icon(
-                  Icons.arrow_downward,
-                  size: 14,
-                  color: Colors.grey.shade500,
+                Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.red,
+                      ),
+                      child: Text(
+                        "무료쿠키!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.cookie),
+                    SizedBox(width: 20),
+                    Icon(Icons.search),
+                  ],
                 ),
               ],
             ),
           ),
+        ),
+
+        // 본문
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 4, 0, 4),
+              child: Row(
+                children: [
+                  Text(
+                    "지금! 인기 급상승",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 4),
+                  Text("남성 TOP 30", style: TextStyle(fontSize: 15)),
+                  SizedBox(width: 165),
+                  Icon(Icons.keyboard_arrow_right, size: 30),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+              child: SizedBox(
+                height: 180,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 30,
+                  itemBuilder: (BuildContext context, int index) {
+                    index = index + 1;
+                    return Column(
+                      children: [
+                        Text("$index"),
+                        Container(
+                          width: 100,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  separatorBuilder: (context, index) => SizedBox(width: 5),
+                ),
+              ),
+              
+            ),
+          ],
         ),
 
         // 바텀 바
